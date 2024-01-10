630d03401918b84231e431e4b9a18713742c3a26
IfeneticTech <inehpaul@gmail.com>
2024-01-10 03:36:04 -0800

    Task 102 of the 0x04-python-more_data_structures project

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    list_keys = list(a_dictionary.keys())
-
-    for value_dicc in list_keys:
-        if value == list(a_dictionary.keys()):
-            del a_dictionary[value_dicc]
-
-    return (a_dictionary)
+def complex_delete(my_dict, value):
+    tmp = my_dict.copy()
+    for w, b in tmp.items():
+        if value == b:
+            my_dict.pop(w)
+    return (my_dict)
