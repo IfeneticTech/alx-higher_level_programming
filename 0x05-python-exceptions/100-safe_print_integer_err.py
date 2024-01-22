6b2c78f6a88e771391fcf52575846512d221050a
IfeneticTech <inehpaul@gmail.com>
2024-01-22 03:14:23 -0800

    Task 100 of the 0x05-python-exception project

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+    except Exception as y:
+        sys.stderr.write("Exception: {}\n".format(y))
+        return (False)
+    else:
+        return (True)
